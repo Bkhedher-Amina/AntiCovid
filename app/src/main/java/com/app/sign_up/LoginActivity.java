f7b04cb79cd35d54ff5ed17d0e1df698706602aa
Amina <aminabkh199@gmail.com>
2020-11-23 18:29:08 +0100

    signup and sign in interface

---
+++ app/src/main/java/com/app/sign_up/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.app.sign_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView btn=findViewById(R.id.textViewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(onNewIntent(LoginActivity.this.RegisterActivity.class));
+            }
+        });
+    }
+}
